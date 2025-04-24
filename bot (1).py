dc1dac9abb53993d7a0013b6f46cad698d31b753
kingsura001 <kingsura001@gmail.com>
2025-04-24 23:37:48 +0530

    Add files via upload

---
+++ bot (1).py
@@ -0,0 +1,31 @@
+from pyrogram import Client
+from pyrogram import idle
+import logging
+import asyncio
+from config import *
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+app = Client(
+    "FileStoreBot",
+    api_id=APP_ID,
+    api_hash=API_HASH,
+    bot_token=TG_BOT_TOKEN,
+    workers=TG_BOT_WORKERS,
+    plugins=dict(root="plugins"),
+)
+
+async def main():
+    await app.start()
+    print(f"Bot Started Powered By @{OWNER_USERNAME}")
+    await idle()
+
+    await app.stop()
+    print("Bot stopped.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
